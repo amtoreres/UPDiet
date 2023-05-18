56f4fab0ea3d9946c6b8828403f07931687c7a24
kmenorio <kmenorio@up.edu.ph>
2023-05-18 14:10:49 +0800

    snapshot of index without notif

--- dashboard/index.php
+++ dashboard/index.php
@@ -57,9 +57,6 @@
         <div class="sidebar">
             <?php //include("../user/overlay_proper.php"); ?>
         </div>
-        <div class="notif">
-            
-        </div>
         <script type="module" src="../js/d_base.js"></script>
         <script type="module" src="../js/d_dashboard.js"></script>
     </body> 
